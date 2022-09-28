f73bf9424f6c3979c860d6dab8030867004a5108
mxwlc <94302084+mxwlc@users.noreply.github.com>
2022-09-28 17:38:27 +0100

    small changes

--- main.cpp
+++ main.cpp
@@ -1,15 +1,4 @@
 #include<iostream>
-class point
-{
-private:
-    /* data */
-    int x,y;
-public:
-    point(int ix, int iy): x(ix) y(iy){}
-    ~point() {
-        std::cout << "Removing from scope\n"
-    }
-};
 
 
 
